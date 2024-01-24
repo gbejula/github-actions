7e9d3857fcd1a1238e34d7782eccbc63025c7cc9
Olugbenga Ajayi <gbejula@gmail.com>
2024-01-24 14:28:16 +0100

    corrected code

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # This is to test actions
 
 def test(a, e):
-    c = a * e
+    return a * e
 
 def test_multiply():
     assert test_multiply(3, 4) == 12
-    assert test_multiply(5, 7) == 35
+    assert test_multiply(5, -7) == -35
