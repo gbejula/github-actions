092f7e00e830836bc3a5e5e6e128286371f7bb2c
Olugbenga Ajayi <gbejula@gmail.com>
2024-01-24 14:59:19 +0100

    renamed

--- main.py
+++
@@ -1,8 +0,0 @@
-# This is to test actions
-
-def test(a, e):
-    return a * e
-
-def test_multiply():
-    assert test(3, 4) == 12
-    assert test(5, 7) == 35
---
+++ multiply.py
@@ -0,0 +1,8 @@
+# This is to test actions
+
+def multiply(a, e):
+    return a * e
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(5, 7) == 35
